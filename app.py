b844eb48d75c20448038a05f58c249ad56879c22
m0rosha <101067195+m0rosha@users.noreply.github.com>
2023-12-16 19:25:54 +0200

    тоха сука мразь

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, session
+from aye import gett_question
+import random as rd 
+
+
+def result():
+    return '<h1>Hello World</h1>'
+def test():
+    
+    data = gett_question(1,session['cnt'])
+    
+    return f"<h4>{str(data)}</h4>"
+def homepage():
+    session['cnt'] = rd.randint(1,2)
+    return '<a href="/test">Test</a>'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] ='BARANOVI4I'
+
+
+
+app.add_url_rule('/','homepage',homepage)
+app.add_url_rule('/result','result', result)
+app.add_url_rule('/test','test', test)
+app.run()
--- aye.py
+++ aye.py
@@ -1,13 +1,16 @@
 import sqlite3
+import random as rd
 
 conn = sqlite3.connect("quiz_1.sqlite")
 
 cursor = conn.cursor()
 
-#cursor.execute("DROP TABLE IF EXISTS quiz")
-##3cursor.execute("DROP TABLE IF EXISTS question")
-#.execute("DROP TABLE IF EXISTS quiz_content")
-#.commit()
+cursor.execute("DROP TABLE IF EXISTS quiz")
+cursor.execute("DROP TABLE IF EXISTS question")
+cursor.execute("DROP TABLE IF EXISTS quiz_content")
+
+cursor.execute("DROP TABLE IF EXISTS quiziz")
+conn.commit()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS quiz (id INTEGER PRIMARY KEY, name VARCHAR)
@@ -33,27 +36,43 @@ CREATE TABLE IF NOT EXISTS quiz_content (
 ''')
 
 
-questions = [
-    ('When ukraine become free','1991','1990','1989','1989'),
-    ('What is the capital of France','Paris','London','Berlin','Lviv')
+quiziz = [("Country quiz",), ("Car quiz",)]
+cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quiziz)
+conn.commit()
 
+questions = [
+    ('When ukraine become free', '1991', '1990', '1989', '1989'),
+    ('What is the capital of France', 'Paris', 'London', 'Berlin', 'Lviv')
 ]
 cursor.executemany('''INSERT INTO question 
                    (question, answer, wrong1, wrong2, wrong3) 
                    VALUES (?, ?, ?, ?, ?)''', questions)
 conn.commit()
-quiziz = [("qweqwe",), ("123123",)]
 
-cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quiziz)
+quiz_content = [
+    (1, 1),
+    (1, 2),
+]
+cursor.executemany('''INSERT INTO quiz_content (quiz_id, question_id) VALUES (?, ?)''', quiz_content)
 conn.commit()
 
-#cursor.execute('''INSERT INTO quiz_content 
- #              (quiz_id, question_id)
-##                VALUES (?,?)''', [2, 1])
-#conn.commit()   
+cursor.execute('''SELECT question.question, question.answer FROM question, quiz_content
+              WHERE question.id == quiz_content.question_id 
+          AND quiz_content.quiz_id == ?
+          AND quiz_content.question_id==?''',[1,rd.randint(1,2)])
 
-cursor.execute('''SELECT question.question, question.answer FROM question, quiz_content 
-               WHERE question.id == quiz_content.question_id 
-               AND quiz_content.quiz_id == ?''', [1])
+
+
+def gett_question(quiz_id, question_id):
+    conn = sqlite3.connect('quiz_1.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('''SELECT question.question, question.answer FROM question, quiz_content
+              WHERE question.id == quiz_content.question_id 
+          AND quiz_content.quiz_id == ?
+          AND quiz_content.question_id == ?''', [question_id,quiz_id])
+   
+    data = cursor.fetchall()
+    return data
+
 data = cursor.fetchall()
-print(data)
+print (data)
